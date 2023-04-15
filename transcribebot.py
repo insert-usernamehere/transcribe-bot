0ac6b222bbd6702b103d5d29a89323ee04aeb1fb
insert-usernamehere <insert@insertapp.net>
2023-04-15 09:42:37 -0400

    more changes

--- transcribebot.py
+++ transcribebot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='.')
 @bot.message_command(name="Transcrible")
 async def transcribe(inter: disnake.ApplicationCommandInteraction, message: disnake.Message):
     # Reversing the message and sending it back.
-    await inter.message.attachments[0].save("audio.mp3")
-    await inter.response.send_message("audio saved")
+    await message.attachments[0].save("audio.mp3")
+    await inter.response.send_message("audio saved", file=disnake.File("audio.mp3"))
 
 bot.run(os.getenv("TOKEN"))
